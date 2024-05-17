454c0c06509e43c16785a51695f48971e08173e1
Alexis Williams <alexis@typedr.at>
2024-05-17 16:56:39 -0700

    Fix incorrect uniform sphere sampling!

--- src/util.rs
+++ src/util.rs
@@ -1,19 +1,22 @@
 use na::{Unit, UnitVector3, Vector3};
 use rand::{thread_rng, Rng};
-use std::f64::consts::PI;
+use std::f64::consts::TAU;
 
 pub fn random_unit_vector() -> UnitVector3<f64> {
+    let u: f64 = 2. * rand::random::<f64>() - 1.;
+    let phi: f64 = TAU * rand::random::<f64>();
+
     Unit::new_normalize(Vector3::new(
-        rand::random::<f64>(),
-        rand::random::<f64>(),
-        rand::random::<f64>(),
+        phi.cos() * (1. - u.powf(2.)).powf(0.5),
+        phi.sin() * (1. - u.powf(2.)).powf(0.5),
+        u,
     ))
 }
 
 pub fn random_in_unit_disk() -> Vector3<f64> {
     let mut rng = thread_rng();
 
-    let theta = rng.gen_range(0. ..2.0 * PI);
+    let theta = rng.gen_range(0. ..TAU);
     let r = rng.gen::<f64>().sqrt();
 
     let x = r * theta.cos();
